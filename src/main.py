60ed5a6733f1a8bae2577f40410e7794efda4c73
Jarno Jellesma <jarnojellesma@gmail.com>
2022-06-29 20:36:10 +0200

    Add TODOs

--- src/main.py
+++ src/main.py
@@ -1,32 +1,35 @@
 from zeroconf import ServiceBrowser, ServiceListener, Zeroconf
 import requests
 import time
-import queue
+import queue as q
 
-q = queue.Queue()
+queue = q.Queue()
 
 
 def send_data_to_server(measurements):
+    # TODO: Failure sending data to server
+    # TODO: Dynamic url to differentiate between raspberries
+    # TODO: API Authorization
     requests.post('http://localhost:5000/', json=measurements)
     print(measurements)
 
 
-def process_q():
+def start_queue_worker():
     while True:
         measurements = []
-        while not q.empty():
-            measurements.append(q.get())
+        while not queue.empty():
+            measurements.append(queue.get())
         send_data_to_server(measurements)
         time.sleep(5)
 
 
-def start(ipaddr):
-    print("ip address:", ipaddr)
+def poll_plug_data(ipaddr):
     while True:
+        # TODO: Failure fetching plug data
         r = requests.get(f"http://{ipaddr}/api/v1/data")
         data = r.json()
         data["timestamp"] = time.time()
-        q.put(data)
+        queue.put(data)
         time.sleep(1)
 
 
@@ -36,11 +39,20 @@ class MyListener(ServiceListener):
         info = zc.get_service_info(type_, name)
         addr = info.addresses[0]
         ipaddr = ".".join([str(b) for b in addr])
-        print(ipaddr)
-        start(ipaddr)
+        print("Connected to smart plug with ip address:", ipaddr)
+        poll_plug_data(ipaddr)
 
 
-zeroconf = Zeroconf()
-listener = MyListener()
-browser = ServiceBrowser(zeroconf, "_hwenergy._tcp.local.", listener)
-process_q()
+def start_smart_plug_poller():
+    # ServiceBrowser is started async
+    # TODO: Connection failure to smart plug
+    ServiceBrowser(Zeroconf(), "_hwenergy._tcp.local.", MyListener())
+
+
+def main():
+    start_smart_plug_poller()
+    start_queue_worker()
+
+
+if __name__ == "__main__":
+    main()
